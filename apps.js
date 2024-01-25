3f57f49d2b7b9f46c2e24a3899288d3d79aa34d1
jordan toujouse <jtoujo1@gmail.com>
2024-01-24 18:06:13 -0600

    created script for my plants display and array.

---
+++ apps.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const flower = {
+  name: "daisy",
+  species: "Gerbera jamesonii",
+  wateringSchedule: "mornings",
+};
+
+const plants = [flower];
+
+const list = document.getElementById("flower-list")
+
+function displayPlants() {
+  list.innerHTML = "";
+  plants.forEach((flower) => {  
+  const p = document.createElement ("p")
+  p.textContent = `${flower.name} ${flower.species} ${flower.wateringSchedule}`
+  list.appendChild (p)
+});
+}
+
+function addPlants(name, species, wateringSchedule) {
+  const newFlower = {name, species, wateringSchedule };
+  plants.push(newFlower);
+}
+
+displayPlants();
