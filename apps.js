5fb8c4445df5dc60b78c084be0ae7035aba97dfc
jordan toujouse <jtoujo1@gmail.com>
2024-01-25 18:53:25 -0600

    fixing js

--- apps.js
+++ apps.js
@@ -6,35 +6,43 @@ const flower = {
   wateringSchedule: "mornings",
 };
 
-const plants = [flower];
+const rawData = localStorage.getItem ('flower')
 
-const list = document.getElementById("flower-list");
+const plants = [''];
+
+plants.push (flower);
+const flowerShop = JSON.stringify(flower);
+localStorage.getItem ("flower, ")
+
+
 
 function displayPlants() {
-  list.innerHTML = "";
-  plants.forEach((flower) => {
-    const p = document.createElement("p");
-    p.textContent = `${flower.name} ${flower.species} ${flower.wateringSchedule}`;
-    list.appendChild(p);
-  });
-}
+  const list = document.getElementById("flower-list");
+  list.innerHTML = ""; 
+
+  plants.forEach(flower) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<p>Name: ${flower.name}</p> 
+    <p>Species: ${flower.species}</p>
+    <p>Watering Schedule: ${flower.wateringSchedule}</p>`
+  }
 
 function addPlants(name, species, wateringSchedule) {
-  const newFlower = { name, species, wateringSchedule };
+  const newFlower = {name, species, wateringSchedule };
   plants.push(newFlower);
-}
+  }
+};
 
 const form = document.getElementById("flower-form");
-form.addEventListener("submit", addPlants(e));
-e.preventDefault();
 
-const name = form.name.value;
-const species = form.species.value;
-const wateringSchedule = form.waterSchedule.value;
+form.addEventListener("submit", (event) => {
+  event.preventDefault ();
 
-// const btn = getElementById("button");
-// btn.addEventListener("click", (event) => {
-// event.addPlants ();
-// }
+  const name = form.name.value;
+  const species = form.species.value;
+  const watering = form.waterSchedule.value;
+
+  addPlants(name, species, watering);
+
 
 displayPlants();
