81b93e6cb8f2687ee2023a7d55710e4f191483e8
jordan toujouse <jtoujo1@gmail.com>
2024-01-25 21:22:09 -0600

    adjusted the script for local storage.

--- apps.js
+++ apps.js
@@ -6,47 +6,59 @@ const flower = {
   wateringSchedule: "mornings",
 };
 
-const rawData = localStorage.getItem ('flower')
-const parsedData = JSON.parse(rawData)
+
 let plants = [];
-plants.push ('flower');
 
 
-plants.push (flower);
-const flowerShop = JSON.stringify(flower);
-localStorage.getItem ("flower, ")
-if (parsedData) {
-  plants = parsedData;
+function loadLocalStage() {
+  const rawData = localStorage.getItem ('plants')
+  const parsedData = JSON.parse(rawData)
+  
+  
+  if (parsedData) {
+    plants = parsedData;
+  } else {
+    addPlants (flower.name, flower.species, flower.wateringSchedule)
+  }
 }
 
 
 function displayPlants() {
   const list = document.getElementById("flower-list");
-  list.innerHTML = ""; 
-
-  plants.forEach(flower) => {
+  list.innerHTML = "";
+  
+  plants.forEach((flower) => {
     const li = document.createElement("li");
     li.innerHTML = `<p>Name: ${flower.name}</p> 
     <p>Species: ${flower.species}</p>
     <p>Watering Schedule: ${flower.wateringSchedule}</p>`
-  }
-
-function addPlants(name, species, wateringSchedule) {
-  const newFlower = {name, species, wateringSchedule };
-  plants.push(newFlower);
-  }
+    list.appendChild (li);
+  })
+  
+  
 };
+  
+  function addPlants(name, species, wateringSchedule) {
+    const newFlower = {name, species, wateringSchedule };
+    plants.push(newFlower);
+    const flowerShop = JSON.stringify(plants);
+    localStorage.setItem ('plants', flowerShop)
+  }
 
 const form = document.getElementById("flower-form");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault ();
-
+  event.preventDefault();
   const name = form.name.value;
   const species = form.species.value;
   const watering = form.waterSchedule.value;
 
   addPlants(name, species, watering);
+  displayPlants ();
+});
 
 
-displayPlants()
+loadLocalStage ();
+
+displayPlants();
+
