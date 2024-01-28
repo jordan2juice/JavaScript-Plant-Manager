da9ee52c52b4efd4bf0a7eacb988006433ef0735
jordan toujouse <jtoujo1@gmail.com>
2024-01-28 13:11:48 -0600

    added style sheets

--- apps.js
+++ apps.js
@@ -6,44 +6,53 @@ const flower = {
   wateringSchedule: "mornings",
 };
 
-
 let plants = [];
 
-
 function loadLocalStage() {
-  const rawData = localStorage.getItem ('plants')
-  const parsedData = JSON.parse(rawData)
-  
-  
+  const rawData = localStorage.getItem("plants");
+  const parsedData = JSON.parse(rawData);
+
   if (parsedData) {
     plants = parsedData;
   } else {
-    addPlants (flower.name, flower.species, flower.wateringSchedule)
+    addPlants(flower.name, flower.species, flower.wateringSchedule);
   }
 }
-
 
 function displayPlants() {
   const list = document.getElementById("flower-list");
   list.innerHTML = "";
-  
+
   plants.forEach((flower) => {
     const li = document.createElement("li");
     li.innerHTML = `<p>Name: ${flower.name}</p> 
     <p>Species: ${flower.species}</p>
-    <p>Watering Schedule: ${flower.wateringSchedule}</p>`
-    list.appendChild (li);
-  })
-  
-  
-};
-  
-  function addPlants(name, species, wateringSchedule) {
-    const newFlower = {name, species, wateringSchedule };
-    plants.push(newFlower);
-    const flowerShop = JSON.stringify(plants);
-    localStorage.setItem ('plants', flowerShop)
-  }
+    <p>Watering Schedule: ${flower.wateringSchedule}</p>`;
+    list.appendChild(li);
+  });
+}
+
+function addPlants(name, species, wateringSchedule) {
+  const newFlower = { name, species, wateringSchedule };
+  plants.push(newFlower);
+  const flowerShop = JSON.stringify(plants);
+  localStorage.setItem("plants", flowerShop);
+}
+
+function removePlant() {
+  plants.pop();
+  const flowerShop = JSON.stringify(plants);
+  localStorage.setItem("plants", flowerShop);
+}
+
+const btn2 = document.getElementById("remove-btn");
+
+btn2.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  removePlant();
+  displayPlants();
+});
 
 const form = document.getElementById("flower-form");
 
@@ -53,12 +62,25 @@ form.addEventListener("submit", (event) => {
   const species = form.species.value;
   const watering = form.waterSchedule.value;
 
+
   addPlants(name, species, watering);
-  displayPlants ();
+  displayPlants();
+
+  if (form.name.value > 5) {
+    event.preventDefault();
+    alert('Not a valid flower name')
+  }
+  if (form.species.value > 3 || form.species.value < 25){
+    event.preventDefault();
+    alert('Not a valid flower species');
+  }
+  if (form.waterSchedule.value > 3 || form.waterSchedule.value < 25){
+    event.preventDefault();
+    alert('Not a valid watering schedule');
+  }
 });
 
 
-loadLocalStage ();
+loadLocalStage();
 
 displayPlants();
-
